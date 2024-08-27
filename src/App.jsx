5fc2254fce1e8068c2957cd19c5c42f039f1769e
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-08-27 20:15:10 +0100

    added cat pics folder and welcome msg on site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-  console.log("hi")
+  return <>
+  
+    <h1>Hello, Jennifer.</h1>
+    <h3>Cat pics will be here soon, I promise.</h3>
+  
+  </>
 }
 
 export default App
