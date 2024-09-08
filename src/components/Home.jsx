b092d9191674676feff47b2025123af8ca23f680
tawheedahmed <tawheedahmed42@outlook.com>
2024-09-08 18:52:54 +0100

    testing if file transfer works with git

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
 
+    function testLog() {
+        console.log("hi")
+    }
+
     return <div id='home'>
 
-        <h1>Felinesweeper</h1>
+        <h1 onClick={testLog}>Felinesweeper</h1>
         <div id='home-difficulty'>
 
             <div id='difficulty-buttons'>
