d3f39a3ad2e6ef65c9d6bbc0361d6bb24c0c732e
tawheedahmed <tawheedahmed42@outlook.com>
2024-10-18 17:51:10 +0100

    minor css changes, imported cat pics - CSS changes needed to display cat pic behind grid properly

--- src/components/Easy.jsx
+++ src/components/Easy.jsx
@@ -20,37 +20,43 @@ export default function Easy() {
     ])
 
 
+    const catPicPath = "/cat-pics/taco"
+    const randomIndex = Math.ceil(Math.random() * 43)
+
     return <>
 
         <div id='easy-page'>
 
             <h1>Felinesweeper</h1>
 
+            <img src={`${catPicPath}${randomIndex}.jpg`} id='hidden-pic'/>
             <div id='easy-grid'>
+            
+
 
                 {grid.map((sq, index) => {
 
-                    return <div className='easy-row' key={`[${index}]`}>
+                    return <div className='easy-row' key={index}>
 
-                    {/* add keys to squares */}
+                        {/* add keys to squares */}
 
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
-                    <div className='easy-square'/>
+                        <div className='easy-square' id={`[${index}][0]`} />
+                        <div className='easy-square' id={`[${index}][1]`} />
+                        <div className='easy-square' id={`[${index}][2]`} />
+                        <div className='easy-square' id={`[${index}][3]`} />
+                        <div className='easy-square' id={`[${index}][4]`} />
+                        <div className='easy-square' id={`[${index}][5]`} />
+                        <div className='easy-square' id={`[${index}][6]`} />
+                        <div className='easy-square' id={`[${index}][7]`} />
+                        <div className='easy-square' id={`[${index}][8]`} />
+                        <div className='easy-square' id={`[${index}][9]`} />
+                        <div className='easy-square' id={`[${index}][10]`} />
+                        <div className='easy-square' id={`[${index}][11]`} />
+                        <div className='easy-square' id={`[${index}][12]`} />
+                        <div className='easy-square' id={`[${index}][13]`} />
+                        <div className='easy-square' id={`[${index}][14]`} />
 
-                </div>
+                    </div>
 
                 })}
 
